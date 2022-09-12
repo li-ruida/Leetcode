d4faefcc40c968adcdfe7e1eb173b6ebc2f7e209
da <26052092328@qq.com>
2022-09-12 11:01:48 +0800

    2022/9/12

---
+++ src/leetcode/CodingInterviews2/algorithm/Offer19.java
@@ -0,0 +1,89 @@
+package leetcode.CodingInterviews2.algorithm;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+/**
+ * @author lrd
+ * @date 2022-09-12 上午8:52
+ * 剑指 Offer 19. 正则表达式匹配
+ * 请实现一个函数用来匹配包含'. '和'*'的正则表达式。模式中的字符'.'表示任意一个字符，而'*'表示它前面的字符可以出现任意次（含0次）。
+ * 在本题中，匹配是指字符串的所有字符匹配整个模式。例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但与"aa.a"和"ab*a"均不匹配。
+ * 示例 1:
+ * 输入:
+ * s = "aa"
+ * p = "a"
+ * 输出: false
+ * 解释: "a" 无法匹配 "aa" 整个字符串。
+ * 示例 2:
+ * 输入:
+ * s = "aa"
+ * p = "a*"
+ * 输出: true
+ * 解释: 因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+ * 示例 3:
+ * 输入:
+ * s = "ab"
+ * p = ".*"
+ * 输出: true
+ * 解释: ".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+ * 示例 4:
+ * 输入:
+ * s = "aab"
+ * p = "c*a*b"
+ * 输出: true
+ * 解释: 因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
+ * 示例 5:
+ * 输入:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * 输出: false
+ * s 可能为空，且只包含从 a-z 的小写字母。
+ * p 可能为空，且只包含从 a-z 的小写字母以及字符 . 和 *，无连续的 '*'。
+ * 注意：本题与主站 10 题相同：https://leetcode-cn.com/problems/regular-expression-matching/
+ */
+public class Offer19 {
+/*
+动态规划
+执行用时：1 ms, 在所有 Java 提交中击败了100.00%的用户
+内存消耗：40 MB, 在所有 Java 提交中击败了72.18%的用户
+ */
+@Leetcode("动态规划")
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] f = new boolean[m + 1][n + 1];//用 f[i][j]表示 s的前i个字符与p中的前j个字符是否能够匹配。在索引1-i存储
+        f[0][0] = true;//动态规划边界,表示字符串可匹配
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    f[i][j] = f[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
+    }
+    public boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);//第i个字符与 第j(非*)或第j-1(*)匹配
+    }
+    @Test
+    public void test(){
+        String s = "mississippi";
+        String p = "mis*is*p*.";
+        System.out.println(isMatch(s,p));
+    }
+}
