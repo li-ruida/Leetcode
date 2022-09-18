771c05d0fe41b8161374a96459012afcbe1f9370
da <26052092328@qq.com>
2022-09-18 17:59:31 +0800

    2022/9/18

---
+++ src/leetcode/CodingInterviews2/algorithm/Offer27.java
@@ -0,0 +1,74 @@
+package leetcode.CodingInterviews2.algorithm;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author lrd
+ * @date 2022-09-17 上午9:19
+ *剑指 Offer 27. 二叉树的镜像
+ * 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+ * 例如输入：
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 镜像输出：
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ * 示例 1：
+ * 输入：root = [4,2,7,1,3,6,9]
+ * 输出：[4,7,2,9,6,3,1]
+ * 限制：
+ * 0 <= 节点个数 <= 1000
+ * 注意：本题与主站 226 题相同：https://leetcode-cn.com/problems/invert-binary-tree/
+ */
+public class Offer27 {
+    public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+/*栈
+执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+内存消耗：39.4 MB, 在所有 Java 提交中击败了5.26%的用户
+ */
+@Leetcode("栈")
+    public TreeNode mirrorTree1(TreeNode root) {
+        if(root==null)
+            return null;
+        Deque stack =new ArrayDeque();
+        stack.add(root);
+        while (!stack.isEmpty()){
+            TreeNode t= (TreeNode) stack.pop();
+            if(t.left != null)
+                stack.add(t.left);
+            if(t.right != null)
+                stack.add(t.right);
+            TreeNode tmp = t.left;
+            t.left = t.right;
+            t.right = tmp;
+        }
+        return root;
+    }
+/*递归
+执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+内存消耗：38.8 MB, 在所有 Java 提交中击败了69.60%的用户
+ */
+@Leetcode("递归")
+    public TreeNode mirrorTree(TreeNode root) {
+        if(root==null)
+            return null;
+        TreeNode left = mirrorTree(root.left);
+        TreeNode right = mirrorTree(root.right);
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
---
+++ src/leetcode/CodingInterviews2/algorithm/Offer28.java
@@ -0,0 +1,91 @@
+package leetcode.CodingInterviews2.algorithm;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author lrd
+ * @date 2022-09-18 下午5:22
+ * 剑指 Offer 28. 对称的二叉树
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ * 示例 1：
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ * 示例 2：
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ * 限制：
+ * 0 <= 节点个数 <= 1000
+ * 注意：本题与主站 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/
+ */
+public class Offer28 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+/*递归
+执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+内存消耗：39.5 MB, 在所有 Java 提交中击败了71.34%的用户
+ */
+@Leetcode("递归")
+    public boolean isSymmetric1(TreeNode root) {
+        return check1(root, root);
+    }
+
+    public boolean check1(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check1(p.left, q.right) && check1(p.right, q.left);
+    }
+/*迭代
+执行用时：1 ms, 在所有 Java 提交中击败了9.62%的用户
+内存消耗：40.8 MB, 在所有 Java 提交中击败了5.03%的用户
+ */
+@Leetcode("队列+迭代")
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+    public boolean check(TreeNode u, TreeNode v) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(u);//添加元素
+        q.offer(v);
+        while (!q.isEmpty()) {
+            u = q.poll();//从队首获取元素，同时获取的这个元素将从原队列删除
+            v = q.poll();
+            if (u == null && v == null) {
+                continue;
+            }
+            if ((u == null || v == null) || (u.val != v.val)) {
+                return false;
+            }
+
+            q.offer(u.left);
+            q.offer(v.right);
+
+            q.offer(u.right);
+            q.offer(v.left);
+        }
+        return true;
+    }
+
+}
