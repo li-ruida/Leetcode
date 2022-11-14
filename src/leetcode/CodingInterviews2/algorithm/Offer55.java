82b651a771c9bb1ad4ba2492a7870ef5550c77b8
da <26052092328@qq.com>
2022-11-14 10:13:05 +0800

    2022/11/14

---
+++ src/leetcode/CodingInterviews2/algorithm/Offer55.java
@@ -0,0 +1,62 @@
+package leetcode.CodingInterviews2.algorithm;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author lrd
+ * @date 2022-11-14 上午9:59
+ * 剑指 Offer 55 - I. 二叉树的深度
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
+ */
+public class Offer55 {
+      public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+@Leetcode("DFS")
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+@Leetcode("BFS")
+    public int maxDepth1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+
+}
