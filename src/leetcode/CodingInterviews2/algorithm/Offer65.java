101aeee569cca1e2524332f386dc95e20c2631f2
da <26052092328@qq.com>
2022-12-25 10:54:03 +0800

    2022/12/25

---
+++ src/leetcode/CodingInterviews2/algorithm/Offer65.java
@@ -0,0 +1,31 @@
+package leetcode.CodingInterviews2.algorithm;
+
+import org.junit.Test;
+
+/**
+ * @author lrd
+ * @date 2022-12-25 10:28
+ * 剑指 Offer 65. 不用加减乘除做加法
+ * 写一个函数，求两个整数之和，要求在函数体内不得使用 “+”、“-”、“*”、“/” 四则运算符号。
+ * 示例:
+ * 输入: a = 1, b = 1
+ * 输出: 2
+ * 提示：
+ * a, b 均可能是负数或 0
+ * 结果不会溢出 32 位整数
+ *
+ * c为当前应该进位的项
+ * a为不进位和,a通过异或消去进位项
+ * b时刻保持为最新的进位项,当无需进位时,结果正确
+ */
+public class Offer65 {
+@Leetcode("数学")
+    public int add(int a, int b) {
+        while(b != 0) { // 当进位为 0 时跳出
+            int c = (a & b) << 1;  // c = 进位
+            a ^= b; // a = 非进位和
+            b = c; // b = 进位
+        }
+        return a;
+    }
+}
