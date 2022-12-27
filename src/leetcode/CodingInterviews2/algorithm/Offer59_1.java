385e4c67a3efb9399637481747b92ee7949eb4c8
da <26052092328@qq.com>
2022-12-27 19:14:53 +0800

    2022/12/27

---
+++ src/leetcode/CodingInterviews2/algorithm/Offer59_1.java
@@ -0,0 +1,56 @@
+package leetcode.CodingInterviews2.algorithm;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author lrd
+ * @date 2022-12-27 17:19
+ * 剑指 Offer 59 - I. 滑动窗口的最大值
+ * 给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
+ * 示例:
+ * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+ * 输出: [3,3,5,5,6,7]
+ * 解释:
+ *   滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ *  1 [3  -1  -3] 5  3  6  7       3
+ *  1  3 [-1  -3  5] 3  6  7       5
+ *  1  3  -1 [-3  5  3] 6  7       5
+ *  1  3  -1  -3 [5  3  6] 7       6
+ *  1  3  -1  -3  5 [3  6  7]      7
+ * 提示：
+ * 你可以假设 k 总是有效的，在输入数组 不为空 的情况下，1 ≤ k ≤ nums.length。
+ * 注意：本题与主站 239 题相同：https://leetcode-cn.com/problems/sliding-window-maximum/
+ *
+ *
+ */
+public class Offer59_1 {
+@Leetcode("队列")
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int len=nums.length;
+        if(len==0)
+            return new int[0];
+        int []ans=new int[len-k+1];
+        Deque<Integer> deque = new LinkedList<>();
+        // 未形成窗口
+        for(int i = 0; i < k; i++) {
+            while(!deque.isEmpty() && deque.peekLast() < nums[i])
+                deque.removeLast();
+            deque.addLast(nums[i]);
+        }
+        ans[0] = deque.peekFirst();
+        // 形成窗口后
+        for(int i = k; i < nums.length; i++) {
+            if(deque.peekFirst() == nums[i - k])//如果左边界为最大值,则移除
+                deque.removeFirst();
+            while(!deque.isEmpty() && deque.peekLast() < nums[i])
+                deque.removeLast();
+            deque.addLast(nums[i]);
+            ans[i - k + 1] = deque.peekFirst();
+        }
+
+        return ans;
+    }
+}
