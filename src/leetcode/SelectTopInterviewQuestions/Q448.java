5c7e7deccdbe900978f3202ece866089a4199471
li-ruida <2605209238@qq.com>
2023-01-12 11:08:49 +0800

    2023/1/12

---
+++ src/leetcode/SelectTopInterviewQuestions/Q448.java
@@ -0,0 +1,43 @@
+package leetcode.SelectTopInterviewQuestions;
+
+import leetcode.Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lrd
+ * @date 2023-01-12 10:08
+ * 448. 找到所有数组中消失的数字
+ * 给你一个含 n 个整数的数组 nums ，其中 nums[i] 在区间 [1, n] 内。请你找出所有在 [1, n] 范围内但没有出现在 nums 中的数字，并以数组的形式返回结果。
+ * 示例 1：
+ * 输入：nums = [4,3,2,7,8,2,3,1]
+ * 输出：[5,6]
+ * 示例 2：
+ * 输入：nums = [1,1]
+ * 输出：[2]
+ * 提示：
+ * n == nums.length
+ * 1 <= n <= 105
+ * 1 <= nums[i] <= n
+ * 进阶：你能在不使用额外空间且时间复杂度为 O(n) 的情况下解决这个问题吗? 你可以假定返回的数组不算在额外空间内。
+ *
+ * 当我们遍历到某个位置时，其中的数可能已经被增加过，因此需要对 nnn 取模来还原出它本来的值
+ */
+public class Q448 {
+    @Leetcode("模拟哈希表")
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        for (int num : nums) {
+            int x = (num - 1) % n;
+            nums[x] += n;
+        }
+        List<Integer> ret = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) {
+                ret.add(i + 1);
+            }
+        }
+        return ret;
+    }
+}
