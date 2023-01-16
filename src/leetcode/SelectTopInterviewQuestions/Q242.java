3b176e61ce0caefcd6a3f11c04cd64cf0e6bf4f0
li-ruida <2605209238@qq.com>
2023-01-16 10:12:45 +0800

    2023/1/16

---
+++ src/leetcode/SelectTopInterviewQuestions/Q242.java
@@ -0,0 +1,39 @@
+package leetcode.SelectTopInterviewQuestions;
+
+import leetcode.Leetcode;
+
+/**
+ * @author lrd
+ * @date 2023-01-16 10:04
+ * 242. 有效的字母异位词
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+ * 示例 1:
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 示例 2:
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ * 提示:
+ * 1 <= s.length, t.length <= 5 * 104
+ * s 和 t 仅包含小写字母
+ */
+public class Q242 {
+    @Leetcode("模拟哈希表")
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] table = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            table[t.charAt(i) - 'a']--;
+            if (table[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
