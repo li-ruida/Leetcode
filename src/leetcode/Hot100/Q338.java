8512a0542ae23df98788034a80282b81eae21b30
li-ruida <2605209238@qq.com>
2023-01-20 09:43:04 +0800

    2023/1/20

---
+++ src/leetcode/Hot100/Q338.java
@@ -0,0 +1,48 @@
+package leetcode.Hot100;
+
+import leetcode.Leetcode;
+
+/**
+ * @author lrd
+ * @date 2023-01-20 9:12
+ * 338. 比特位计数
+ * 给你一个整数 n ，对于 0 <= i <= n 中的每个 i ，计算其二进制表示中 1 的个数 ，返回一个长度为 n + 1 的数组 ans 作为答案。
+ * 示例 1：
+ * 输入：n = 2
+ * 输出：[0,1,1]
+ * 解释：
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * 示例 2：
+ * 输入：n = 5
+ * 输出：[0,1,1,2,1,2]
+ * 解释：
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * 3 --> 11
+ * 4 --> 100
+ * 5 --> 101
+ * 提示：
+ * 0 <= n <= 105
+ */
+public class Q338 {
+    @Leetcode("n&(n-1)")
+    public int[] countBits(int n) {
+        int[] bits = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            bits[i] = countOnes(i);
+        }
+        return bits;
+    }
+
+    public int countOnes(int x) {
+        int ones = 0;
+        while (x > 0) {
+            x &= (x - 1);
+            ones++;
+        }
+        return ones;
+    }
+}
