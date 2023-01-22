6ba678d5c9943ad412b651adbd73d02da4ebf09f
li-ruida <2605209238@qq.com>
2023-01-22 09:26:40 +0800

    2023/1/22

--- src/leetcode/Hot100/Q322.java
+++ src/leetcode/Hot100/Q322.java
@@ -38,7 +38,7 @@ public class Q322 {
         for (int i = 0; i < coins.length; i++) {
             //正序遍历：完全背包每个硬币可以选择多次
             for (int j = coins[i]; j <= amount; j++) {
-                //只有dp[j-coins[i]]不是初始最大值时，该位才有选择的必要
+                //只有dp[j-coins[i]]不是初始最大值时，j-coins[i]才是能凑出的金额,该位有意义
                 if (dp[j - coins[i]] != max) {
                     //选择硬币数目最小的情况
                     dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
