aeb4ed9e4f0346a9a61f5f092ede10ed3c421f96
li-ruida <2605209238@qq.com>
2023-02-02 09:15:39 +0800

    2023/2/2

---
+++ src/leetcode/codetop/Q139.java
@@ -0,0 +1,48 @@
+package leetcode.codetop;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author lrd
+ * @date 2023-02-02 8:39
+ * 139. 单词拆分
+ * 给你一个字符串 s 和一个字符串列表 wordDict 作为字典。请你判断是否可以利用字典中出现的单词拼接出 s 。
+ * 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+ * 示例 1：
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+ * 示例 2：
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+ *      注意，你可以重复使用字典中的单词。
+ * 示例 3：
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ * 提示：
+ * 1 <= s.length <= 300
+ * 1 <= wordDict.length <= 1000
+ * 1 <= wordDict[i].length <= 20
+ * s 和 wordDict[i] 仅有小写英文字母组成
+ * wordDict 中的所有字符串 互不相同
+ */
+public class Q139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int len=s.length();
+        boolean []dp=new boolean[len+1];
+        Set<String> set=new HashSet<>(wordDict);
+        dp[0]=true;//表示空串
+        for (int i = 1; i <= len; i++) {//dp[i]为0 - i-1是否合法
+            for (int j = 0; j < i; j++) {//表示任意分隔长度
+                if(dp[j]&&set.contains(s.substring(j,i))){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[len];
+    }
+}
