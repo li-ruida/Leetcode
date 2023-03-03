2bb41de7fe66d22df48fc21420a5e81c8a52881e
li-ruida <2605209238@qq.com>
2023-03-03 09:34:27 +0800

    2023/3/3

---
+++ src/leetcode/codetop/Q5.java
@@ -0,0 +1,85 @@
+package leetcode.codetop;
+
+import leetcode.Leetcode;
+
+/**
+ * @author lrd
+ * @date 2023-03-03 8:43
+ * 5. 最长回文子串
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ * 如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+ * 示例 1：
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * 示例 2：
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ * 提示：
+ * 1 <= s.length <= 1000
+ * s 仅由数字和英文字母组成
+ */
+public class Q5 {
+    @Leetcode("中心扩散法 动态规划")
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 2)
+            return s;
+        int strlen = s.length();
+        int start = 0;
+        int end = 0;
+        int len = 1;
+        boolean[][] dp = new boolean[strlen][strlen];
+        for (int r = 0; r < strlen; r++) {
+            for (int l = 0; l < r; l++) {
+                if (s.charAt(l) == s.charAt(r) && (r - l <= 2 || dp[l + 1][r - 1])) {
+                    dp[l][r] = true;
+                    if (r - l + 1 > len) {
+                        len = r - l + 1;
+                        start = l;
+                        end = r;
+                    }
+                }
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public String longestPalindrome1(String s) {
+
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        int strLen = s.length();
+        int left = 0;
+        int right = 0;
+        int len = 1;
+        int maxStart = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < strLen; i++) {
+            left = i - 1;
+            right = i + 1;
+            while (left >= 0 && s.charAt(left) == s.charAt(i)) {
+                len++;
+                left--;
+            }
+            while (right < strLen && s.charAt(right) == s.charAt(i)) {
+                len++;
+                right++;
+            }
+            while (left >= 0 && right < strLen && s.charAt(right) == s.charAt(left)) {
+                len = len + 2;
+                left--;
+                right++;
+            }
+            if (len > maxLen) {
+                maxLen = len;
+                maxStart = left;
+            }
+            len = 1;
+        }
+        return s.substring(maxStart + 1, maxStart + maxLen + 1);
+
+    }
+
+}
