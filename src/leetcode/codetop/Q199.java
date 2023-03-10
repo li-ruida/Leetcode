22776b3bb4946f6af6ed2155f5e86dc7ceb33cee
li-ruida <2605209238@qq.com>
2023-03-10 10:23:13 +0800

    2023/3/10

---
+++ src/leetcode/codetop/Q199.java
@@ -0,0 +1,79 @@
+package leetcode.codetop;
+
+import leetcode.Leetcode;
+
+import java.util.*;
+
+/**
+ * @author lrd
+ * @date 2023-03-10 9:49
+ * 199. 二叉树的右视图
+ * 给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * <p>
+ * <p>
+ * 输入: [1,2,3,null,5,null,4]
+ * 输出: [1,3,4]
+ * 示例 2:
+ * <p>
+ * 输入: [1,null,3]
+ * 输出: [1,3]
+ * 示例 3:
+ * <p>
+ * 输入: []
+ * 输出: []
+ * <p>
+ * <p>
+ * 提示:
+ * <p>
+ * 二叉树的节点个数的范围是 [0,100]
+ * -100 <= Node.val <= 100
+ */
+public class Q199 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    @Leetcode("BFS")
+    List<Integer> ans=new ArrayList<>();
+    Deque<TreeNode> list=new ArrayDeque<>();
+    public List<Integer> rightSideView(TreeNode root) {
+        bfs(root);
+        return ans;
+    }
+    void bfs(TreeNode root){
+        if(root==null)
+            return;
+        list.offer(root);
+        while (!list.isEmpty()){
+            ans.add(list.peekLast().val);
+            int cursize=list.size();
+            for (int i = 0; i < cursize; i++) {
+                TreeNode tmp=list.pollFirst();
+                if(tmp.left!=null)
+                    list.offer(tmp.left);
+                if(tmp.right!=null)
+                    list.offer(tmp.right);
+            }
+        }
+    }
+}
