a6615e18c0ad47cb5f858c75f5f000dfcc1a8aa5
li-ruida <2605209238@qq.com>
2023-04-02 11:16:46 +0800

    2023/4/2

---
+++ src/leetcode/codetop/Q240.java
@@ -0,0 +1,72 @@
+package leetcode.codetop;
+
+import leetcode.Leetcode;
+
+/**
+ * @author lrd
+ * @date 2023-04-02 10:41
+ * 240. 搜索二维矩阵 II
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ * 示例 1：
+ * 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+ * 输出：true
+ * 示例 2：
+ * 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+ * 输出：false
+ * 提示：
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= n, m <= 300
+ * -109 <= matrix[i][j] <= 109
+ * 每行的所有元素从左到右升序排列
+ * 每列的所有元素从上到下升序排列
+ * -109 <= target <= 109
+ */
+public class Q240 {
+    @Leetcode("二分查找")
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (int[] row : matrix) {
+            int index = search(row, target);
+            if (index >= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    //二分查找每次搜索可以排除半行或半列的元素，Z字形查找每次搜索可以排除一行或一列的元素
+    public boolean searchMatrix1(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int x = 0, y = n - 1;
+        while (x < m && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            }
+            if (matrix[x][y] > target) {
+                --y;
+            } else {
+                ++x;
+            }
+        }
+        return false;
+    }
+
+}
