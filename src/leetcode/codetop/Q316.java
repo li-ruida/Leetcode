f3144ee03199509361b19e8431e6fbafd36a3eff
li-ruida <2605209238@qq.com>
2023-04-03 11:31:20 +0800

    2023/4/3

---
+++ src/leetcode/codetop/Q316.java
@@ -0,0 +1,50 @@
+package leetcode.codetop;
+
+import leetcode.Leetcode;
+
+/**
+ * @author lrd
+ * @date 2023-04-03 10:11
+ * 316. 去除重复字母
+ * 给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。
+ * 需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）。
+ * 示例 1：
+ * 输入：s = "bcabc"
+ * 输出："abc"
+ * 示例 2：
+ * 输入：s = "cbacdcbc"
+ * 输出："acdb"
+ * 提示：
+ * 1 <= s.length <= 104
+ * s 由小写英文字母组成
+ * 注意：该题与 1081 https://leetcode-cn.com/problems/smallest-subsequence-of-distinct-characters 相同
+ */
+public class Q316 {
+    @Leetcode("贪心 单调栈")
+    public String removeDuplicateLetters(String s) {
+        boolean[] vis = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!vis[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        vis[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                vis[ch - 'a'] = true;
+                sb.append(ch);
+            }
+            num[ch - 'a'] -= 1;
+        }
+        return sb.toString();
+    }
+}
