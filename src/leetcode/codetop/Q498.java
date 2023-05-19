3ca4c1022b50749c0fb6c3c19c7e18db26ea716e
li-ruida <2605209238@qq.com>
2023-05-19 08:04:08 +0800

    2023/5/19

---
+++ src/leetcode/codetop/Q498.java
@@ -0,0 +1,50 @@
+package leetcode.codetop;
+
+/**
+ * @author lrd
+ * @date 2023-05-19 7:48
+ * 498. 对角线遍历
+ * 给你一个大小为 m x n 的矩阵 mat ，请以对角线遍历的顺序，用一个数组返回这个矩阵中的所有元素。
+ * 示例 1：
+ * 输入：mat = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[1,2,4,7,5,3,6,8,9]
+ * 示例 2：
+ * 输入：mat = [[1,2],[3,4]]
+ * 输出：[1,2,3,4]
+ * 提示：
+ * m == mat.length
+ * n == mat[i].length
+ * 1 <= m, n <= 104
+ * 1 <= m * n <= 104
+ * -105 <= mat[i][j] <= 105
+ */
+public class Q498 {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] res = new int[m * n];
+        int pos = 0;
+        for (int i = 0; i < m + n - 1; i++) {
+            if (i % 2 == 1) {
+                int x = i < n ? 0 : i - n + 1;
+                int y = i < n ? i : n - 1;
+                while (x < m && y >= 0) {
+                    res[pos] = mat[x][y];
+                    pos++;
+                    x++;
+                    y--;
+                }
+            } else {
+                int x = i < m ? i : m - 1;
+                int y = i < m ? 0 : i - m + 1;
+                while (x >= 0 && y < n) {
+                    res[pos] = mat[x][y];
+                    pos++;
+                    x--;
+                    y++;
+                }
+            }
+        }
+        return res;
+    }
+}
