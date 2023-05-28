969c4388c834293fc7d878dcabdc6104d1677d0b
li-ruida <2605209238@qq.com>
2023-05-28 09:29:35 +0800

    2023/5/28

---
+++ src/leetcode/codetop/Q416.java
@@ -0,0 +1,51 @@
+package leetcode.codetop;
+
+import leetcode.Leetcode;
+
+/**
+ * @author lrd
+ * @date 2023-05-28 8:50
+ * 416. 分割等和子集
+ * 给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+ * 示例 1：
+ * 输入：nums = [1,5,11,5]
+ * 输出：true
+ * 解释：数组可以分割成 [1, 5, 5] 和 [11] 。
+ * 示例 2：
+ * 输入：nums = [1,2,3,5]
+ * 输出：false
+ * 解释：数组不能分割成两个元素和相等的子集。
+ * 提示：
+ * 1 <= nums.length <= 200
+ * 1 <= nums[i] <= 100
+ */
+public class Q416 {
+    @Leetcode("动态规划")
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return false;
+        }
+        int sum = 0, maxNum = 0;
+        for (int num : nums) {
+            sum += num;
+            maxNum = Math.max(maxNum, num);
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        if (maxNum > target) {
+            return false;
+        }
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            for (int j = target; j >= num; --j) {
+                dp[j] |= dp[j - num];
+            }
+        }
+        return dp[target];
+    }
+}
