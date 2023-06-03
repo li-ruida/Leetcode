aeec1d5b23dd401f1fbb3c4a1fe3059f7a2e5af7
li-ruida <2605209238@qq.com>
2023-06-03 11:34:26 +0800

    2023/6/2

---
+++ src/leetcode/daimasuixianglu/Q34.java
@@ -0,0 +1,58 @@
+package leetcode.daimasuixianglu;
+
+import leetcode.Leetcode;
+
+/**
+ * @author lrd
+ * @date 2023-06-03 10:20
+ * 34. 在排序数组中查找元素的第一个和最后一个位置
+ * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+ * 示例 1：
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ * 示例 2：
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ * 示例 3：
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ * 提示：
+ * 0 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ * nums 是一个非递减数组
+ * -109 <= target <= 109
+ */
+public class Q34 {
+    @Leetcode("二分")
+    public int[] searchRange(int[] nums, int target) {
+        return new int[]{search(nums,target,true),search(nums,target,false)};
+    }
+    public int search(int []nums,int target,boolean left){
+        int len=nums.length;
+        int l=0,r=len-1;
+        while(l<=r){
+            int mid=(r-l)/2+l;
+            if(nums[mid]>target)
+                r=mid-1;
+            else if(nums[mid]<target)
+                l=mid+1;
+            else{
+                if(left){
+                    while(mid>l&&nums[mid]==nums[mid-1]){
+                        mid--;
+                    }
+                    return mid;
+                }
+                else{
+                    while(mid<r&&nums[mid]==nums[mid+1]){
+                        mid++;
+                    }
+                    return mid;
+                }
+            }
+        }
+        return -1;
+    }
+}
