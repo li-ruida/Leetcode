ac1428b3fb842d6772ff1e43d22ea4344a4ff154
li-ruida <2605209238@qq.com>
2023-07-13 08:27:17 +0800

    2023/7/13

---
+++ src/leetcode/daimasuixianglu/Q404.java
@@ -0,0 +1,33 @@
+package leetcode.daimasuixianglu;
+
+/**
+ * @author lrd
+ * @date 2023-07-13 8:24
+ * 404. 左叶子之和
+ * 给定二叉树的根节点 root ，返回所有左叶子之和。
+ * 示例 1：
+ * 输入: root = [3,9,20,null,null,15,7]
+ * 输出: 24
+ * 解释: 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ * 示例 2:
+ * 输入: root = [1]
+ * 输出: 0
+ * 提示:
+ * 节点数在 [1, 1000] 范围内
+ * -1000 <= Node.val <= 1000
+ */
+public class Q404 {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int sum = 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
+        }
+        sum += sumOfLeftLeaves(root.left);
+        sum += sumOfLeftLeaves(root.right);
+        return sum;
+    }
+}
