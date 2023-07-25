c700e8b81527906c66f2463ba8fd02f9e9b2236f
li-ruida <2605209238@qq.com>
2023-07-25 09:26:54 +0800

    2023/7/25

---
+++ src/leetcode/daimasuixianglu/Q216.java
@@ -0,0 +1,65 @@
+package leetcode.daimasuixianglu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lrd
+ * @date 2023-07-25 9:24
+ * 216. 组合总和 III
+ * 找出所有相加之和为 n 的 k 个数的组合，且满足下列条件：
+ * 只使用数字1到9
+ * 每个数字 最多使用一次
+ * 返回 所有可能的有效组合的列表 。该列表不能包含相同的组合两次，组合可以以任何顺序返回。
+ * 示例 1:
+ * 输入: k = 3, n = 7
+ * 输出: [[1,2,4]]
+ * 解释:
+ * 1 + 2 + 4 = 7
+ * 没有其他符合的组合了。
+ * 示例 2:
+ * 输入: k = 3, n = 9
+ * 输出: [[1,2,6], [1,3,5], [2,3,4]]
+ * 解释:
+ * 1 + 2 + 6 = 9
+ * 1 + 3 + 5 = 9
+ * 2 + 3 + 4 = 9
+ * 没有其他符合的组合了。
+ * 示例 3:
+ * 输入: k = 4, n = 1
+ * 输出: []
+ * 解释: 不存在有效的组合。
+ * 在[1,9]范围内使用4个不同的数字，我们可以得到的最小和是1+2+3+4 = 10，因为10 > 1，没有有效的组合。
+ * 提示:
+ * 2 <= k <= 9
+ * 1 <= n <= 60
+ */
+public class Q216 {
+    List<Integer> temp = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        dfs(1, 9, k, n);
+        return ans;
+    }
+
+    public void dfs(int cur, int n, int k, int sum) {
+        if (temp.size() + (n - cur + 1) < k || temp.size() > k) {
+            return;
+        }
+        if (temp.size() == k) {
+            int tempSum = 0;
+            for (int num : temp) {
+                tempSum += num;
+            }
+            if (tempSum == sum) {
+                ans.add(new ArrayList<Integer>(temp));
+                return;
+            }
+        }
+        temp.add(cur);
+        dfs(cur + 1, n, k, sum);
+        temp.remove(temp.size() - 1);
+        dfs(cur + 1, n, k, sum);
+    }
+}
