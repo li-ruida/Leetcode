8496eb2f057b68d2d2352346f0c44205d0c1c973
li-ruida <2605209238@qq.com>
2023-08-20 10:33:25 +0800

    2023/8/20

---
+++ src/leetcode/daimasuixianglu/Q435.java
@@ -0,0 +1,53 @@
+package leetcode.daimasuixianglu;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author lrd
+ * @date 2023-08-20 10:01
+ * 435. 无重叠区间
+ * 给定一个区间的集合 intervals ，其中 intervals[i] = [starti, endi] 。
+ * 返回 需要移除区间的最小数量，使剩余区间互不重叠 。
+ * 示例 1:
+ * 输入: intervals = [[1,2],[2,3],[3,4],[1,3]]
+ * 输出: 1
+ * 解释: 移除 [1,3] 后，剩下的区间没有重叠。
+ * 示例 2:
+ * 输入: intervals = [ [1,2], [1,2], [1,2] ]
+ * 输出: 2
+ * 解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
+ * 示例 3:
+ * 输入: intervals = [ [1,2], [2,3] ]
+ * 输出: 0
+ * 解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
+ * 提示:
+ * 1 <= intervals.length <= 105
+ * intervals[i].length == 2
+ * -5 * 104 <= starti < endi <= 5 * 104
+ */
+public class Q435 {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] interval1, int[] interval2) {
+                return interval1[1] - interval2[1];
+            }
+        });
+
+        int n = intervals.length;
+        int right = intervals[0][1];
+        int ans = 1;
+        for (int i = 1; i < n; ++i) {
+            if (intervals[i][0] >= right) {
+                ++ans;
+                right = intervals[i][1];
+            }
+        }
+        return n - ans;
+    }
+
+}
