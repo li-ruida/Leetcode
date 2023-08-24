465f855f1a0db69075dd4ddb2ddfedf9f25b50c1
li-ruida <2605209238@qq.com>
2023-08-24 09:34:47 +0800

    2023/8/24

---
+++ src/leetcode/daimasuixianglu/Q763.java
@@ -0,0 +1,45 @@
+package leetcode.daimasuixianglu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lrd
+ * @date 2023-08-24 9:29
+ * 763. 划分字母区间
+ * 给你一个字符串 s 。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。
+ * 注意，划分结果需要满足：将所有划分结果按顺序连接，得到的字符串仍然是 s 。
+ * 返回一个表示每个字符串片段的长度的列表。
+ * 示例 1：
+ * 输入：s = "ababcbacadefegdehijhklij"
+ * 输出：[9,7,8]
+ * 解释：
+ * 划分结果为 "ababcbaca"、"defegde"、"hijhklij" 。
+ * 每个字母最多出现在一个片段中。
+ * 像 "ababcbacadefegde", "hijhklij" 这样的划分是错误的，因为划分的片段数较少。
+ * 示例 2：
+ * 输入：s = "eccbbbbdec"
+ * 输出：[10]
+ * 提示：
+ * 1 <= s.length <= 500
+ * s 仅由小写英文字母组成
+ */
+public class Q763 {
+    public List<Integer> partitionLabels(String s) {
+        int[] last = new int[26];
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
+        List<Integer> partition = new ArrayList<Integer>();
+        int start = 0, end = 0;
+        for (int i = 0; i < length; i++) {
+            end = Math.max(end, last[s.charAt(i) - 'a']);
+            if (i == end) {
+                partition.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return partition;
+    }
+}
