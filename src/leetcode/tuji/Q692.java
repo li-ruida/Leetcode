8b08e19756306b4171245fb1ed5f62e304d553f0
li-ruida <2605209238@qq.com>
2023-09-02 15:36:12 +0800

    2023/9/2

---
+++ src/leetcode/tuji/Q692.java
@@ -0,0 +1,51 @@
+package leetcode.tuji;
+
+import java.util.*;
+
+/**
+ * @author lrd
+ * @date 2023-09-02 15:31
+ * 692. 前K个高频单词
+ * 给定一个单词列表 words 和一个整数 k ，返回前 k 个出现次数最多的单词。
+ * 返回的答案应该按单词出现频率由高到低排序。如果不同的单词有相同出现频率， 按字典顺序 排序。
+ * 示例 1：
+ * 输入: words = ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+ * 输出: ["i", "love"]
+ * 解析: "i" 和 "love" 为出现次数最多的两个单词，均为2次。
+ *     注意，按字母顺序 "i" 在 "love" 之前。
+ * 示例 2：
+ * 输入: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+ * 输出: ["the", "is", "sunny", "day"]
+ * 解析: "the", "is", "sunny" 和 "day" 是出现次数最多的四个单词，
+ *     出现次数依次为 4, 3, 2 和 1 次。
+ * 注意：
+ * 1 <= words.length <= 500
+ * 1 <= words[i] <= 10
+ * words[i] 由小写英文字母组成。
+ * k 的取值范围是 [1, 不同 words[i] 的数量]
+ */
+public class Q692 {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> cnt = new HashMap<String, Integer>();
+        for (String word : words) {
+            cnt.put(word, cnt.getOrDefault(word, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<Map.Entry<String, Integer>>(new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
+                return entry1.getValue() == entry2.getValue() ? entry2.getKey().compareTo(entry1.getKey()) : entry1.getValue() - entry2.getValue();
+            }
+        });
+        for (Map.Entry<String, Integer> entry : cnt.entrySet()) {
+            pq.offer(entry);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        List<String> ret = new ArrayList<String>();
+        while (!pq.isEmpty()) {
+            ret.add(pq.poll().getKey());
+        }
+        Collections.reverse(ret);
+        return ret;
+    }
+}
